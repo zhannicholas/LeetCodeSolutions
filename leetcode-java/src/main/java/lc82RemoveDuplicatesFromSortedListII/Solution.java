7a66ff95ee0c418274389714625e90662c05565b
Nicholas Zhan <zhan_nicholas@outlook.com>
2021-03-25 20:24:48 +0800

    Add 82: remove duplicates from sorted list II.

---
+++ leetcode-java/src/main/java/lc82RemoveDuplicatesFromSortedListII/Solution.java
@@ -0,0 +1,56 @@
+package lc82RemoveDuplicatesFromSortedListII;
+
+/*
+82. 删除排序链表中的重复元素 II
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+
+返回同样按升序排列的结果链表。
+
+
+
+示例 1：
+
+
+输入：head = [1,2,3,3,4,4,5]
+输出：[1,2,5]
+示例 2：
+
+
+输入：head = [1,1,1,2,3]
+输出：[2,3]
+
+
+提示：
+
+链表中节点数目在范围 [0, 300] 内
+-100 <= Node.val <= 100
+题目数据保证链表已经按升序排列
+*/
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int val = cur.next.val;
+                while (cur.next != null && cur.next.val == val) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
