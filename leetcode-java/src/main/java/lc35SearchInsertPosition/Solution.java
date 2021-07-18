05faf4a11c058c399b2a87a31770b69cf8d7df07
Nicholas Zhan <zhan_nicholas@outlook.com>
2021-07-18 08:56:47 +0800

    添加：35. 搜索插入位置

---
+++ leetcode-java/src/main/java/lc35SearchInsertPosition/Solution.java
@@ -0,0 +1,40 @@
+package lc35SearchInsertPosition;
+
+/*
+35. 搜索插入位置
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+你可以假设数组中无重复元素。
+
+示例 1:
+
+输入: [1,3,5,6], 5
+输出: 2
+示例 2:
+
+输入: [1,3,5,6], 2
+输出: 1
+示例 3:
+
+输入: [1,3,5,6], 7
+输出: 4
+ */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
