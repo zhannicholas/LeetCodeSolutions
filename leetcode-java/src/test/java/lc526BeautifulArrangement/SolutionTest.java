f115c363f98241178107e333b7021ebe6aca08ed
Nicholas Zhan <zhan_nicholas@outlook.com>
2021-08-16 08:48:51 +0800

    添加：526. 优美的排列

---
+++ leetcode-java/src/test/java/lc526BeautifulArrangement/SolutionTest.java
@@ -0,0 +1,14 @@
+package lc526BeautifulArrangement;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void countArrangement() {
+        Solution solution = new Solution();
+        assertEquals(2, solution.countArrangement(2));
+    }
+}
